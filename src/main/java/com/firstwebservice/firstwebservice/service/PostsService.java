66e2ac04d59eb7d75ffff6f6221e40eb00265ae9
KING_MASS <03337@naver.com>
2022-02-06 01:46:31 +0900

    106

---
+++ src/main/java/com/firstwebservice/firstwebservice/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.firstwebservice.firstwebservice.service;
+
+import com.firstwebservice.firstwebservice.domain.posts.PostsRepository;
+import com.firstwebservice.firstwebservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
